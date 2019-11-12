b03c91f2cf35111567e65ad1b4982a8b07a28ec1
Amy Bemister <amy.bemister@gmail.com>
2019-11-12 00:50:50 +0000

    Query to get the total number of assignments for each day of bootcamp

---
+++ 3_queries_group-by/1_assignments_per_day.sql
@@ -0,0 +1,10 @@
+SELECT day, COUNT(id) AS total_assignments
+FROM assignments
+GROUP BY day
+ORDER BY day;
+
+-- COMPASS ANSWER - SAME RESULT
+-- SELECT day, count(*) as total_assignments 
+-- FROM assignments
+-- GROUP BY day
+-- ORDER BY day;
