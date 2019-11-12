e35335b51936bbab96ffb7f6576c78075a4c5e6a
Amy Bemister <amy.bemister@gmail.com>
2019-11-12 20:06:27 +0000

    Query to get the average duration of assistance requests for each cohort

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,15 @@
+SELECT cohorts.name AS name, AVG(started_at - completed_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC;
+
+-- COMPASS ANSWER - SAME RESULT
+
+-- SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+-- FROM assistance_requests 
+-- JOIN students ON students.id = assistance_requests.student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY average_assistance_time;
